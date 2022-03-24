051dcea16bbca5ae83da14a3e0e3cbb5f936048a
CPalileo <cpalileo@gmail.com>
2022-03-24 15:40:08 -0700

    setup schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+
+DROP DATABASE IF EXISTS emp_track;
+
+CREATE DATABASE emp_track;
+
+USE emp_track;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR (30),
+);
+
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR (30),
+  salary DECIMAL(10,2),
+  department_id INTEGER
+);
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR (30),
+  last_name VARCHAR (30),
+  role_id INTEGER,
+  manager_id INTEGER
+);
+
